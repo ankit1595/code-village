b716f25e3f7f8aa21982955759ffee88a5843d50
Ankit <ankit.95sharma@gmail.com>
2022-05-28 02:49:19 +0530

    set up partials and express-ejs-layouts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const port = 8000;
 const app = express();
 
+app.use(expressLayouts);
 app.use("/", require("./routes/index"));
 
 //set up the view engine
