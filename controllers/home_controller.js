6a70ab8b78c002850bbcf356f5d2a571ac027b35
Ankit <ankit.95sharma@gmail.com>
2022-06-19 03:22:05 +0530

    Nested population: display comment and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -11,6 +11,12 @@ module.exports.home = function (req, res) {
   // });
   Post.find({})
     .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    })
     .exec(function (err, posts) {
       return res.render("home", {
         title: "Home",
