b7a5c742adbf8d18cbf8c8d314abf4948e970cb0
Ankit <ankit.95sharma@gmail.com>
2022-06-26 05:06:24 +0530

    file upload: replace avatar file in disk storage

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const fs = require("fs");
+const path = require("path");
 
 module.exports.profile = function (req, res) {
   User.findById(req.params.id, function (err, user) {
@@ -29,6 +31,9 @@ module.exports.update = async function (req, res) {
         user.name = req.body.name;
         user.email = req.body.email;
         if (req.file) {
+          if (user.avatar) {
+            fs.unlinkSync(path.join(__dirname, "..", user.avatar));
+          }
           // this is saving the path of the uploaded file into the avatar field of the User Database
           user.avatar = User.avatarPath + "/" + req.file.filename;
         }
