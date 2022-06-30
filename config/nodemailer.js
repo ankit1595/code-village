c66645976a99599039b403ac1a55a10ed0913bf5
Ankit <ankit.95sharma@gmail.com>
2022-06-30 23:57:16 +0530

    Mailer: Setup nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const Logger = require("nodemon/lib/utils/log");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "ankit.95sharma@gmail.com",
+    pass: "aisekaisebatadun",
+  },
+});
+
+let renderTemplate = (data, relativePath) => {
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("error in rendering template");
+        return;
+      }
+      mailHTML = template;
+    }
+  );
+  return mailHTML;
+};
+
+module.exports = {
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
